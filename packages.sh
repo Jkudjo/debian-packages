2e0e99c280993d43488e13370025da0bf29a41c1
Philip Kumah Jr <philipkumahjr@gmail.com>
2023-05-26 01:22:42 +0000

    Update packages.sh
    chg: added gh cli

--- packages.sh
+++ packages.sh
@@ -4,6 +4,13 @@
 # Install Curl
 sudo apt install -y curl
 
+#install gh cli for github
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 
 #For Oh-my-zsh shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
